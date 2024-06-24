6d270d499135d29b4de14225b5df412b94c9a798
Jack Kraus <jackkraus3856@gmail.com>
2024-06-24 17:16:08 -0400

    Changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home.tsx';
 import './css/index.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar.tsx';
 import Footer from './components/Footer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
+    <Home />
     <Footer/>
   </React.StrictMode>,
 )
